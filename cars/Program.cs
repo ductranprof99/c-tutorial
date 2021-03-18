a0b2290b206ada1c78d140ad6eba4a973b3a298f
duc tran <ducprofessorx@gmail.com>
2021-03-18 12:52:37 +0700

    first and where

--- cars/Program.cs
+++ cars/Program.cs
@@ -12,12 +12,21 @@ namespace cars
         static void Main(string[] args)
         {
             var cars = ProcessFile("D:\\C#proj\\Tutorial\\L2.Linq\\cars\\fuel.csv");
-            var quering = cars.OrderByDescending(c=>c.Combined);
-
+            var quering = from car in cars 
+                          orderby car.Combined ascending, car.Name descending
+                          select car; 
+            //dung cai nay hay hon
+            var top = cars.OrderByDescending(c=>c.Name)
+                          .ThenBy(c=>c.Combined)
+                          .Select(c=>c)
+                          .First(c=> c.Manufacturer == "BMW" && c.Year == 2016);
+            // neu tim k thay thi bi fail -> bao loi
+            // neu co xay ra truong hop top == null thi dung method FirstorDefault thay First
             foreach (var item in quering.Take(10))
             {
                 Console.WriteLine($"{item.Name} : {item.Combined}");
             }
+            Console.WriteLine($"{top.Name} : {top.Combined}");
         }
         private static List<Car> ProcessFile (string path)
         {
